45504e98cad02c08855af18a54fd2861a36727e7
Nandha Kumar <nandha2447@gmail.com>
2018-10-29 00:04:57 +0530

    First commit for mobile flashcards

---
+++ screens/LinksScreen.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { ScrollView, 
+         StyleSheet, 
+         TouchableOpacity,
+         Text,
+         TextInput
+       } from 'react-native';
+//import { ExpoLinksView } from '@expo/samples';
+
+export default class LinksScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Create a new Deck',
+    headerStyle: {
+      backgroundColor: '#f4511e',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+  };
+
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        {/* Go ahead and delete ExpoLinksView and replace it with your
+           * content, we just wanted to provide you with some helpful links */}
+        <Text>What's the title of your new deck?</Text>
+        <TextInput 
+          placeholder="New title of the deck" 
+        />
+        <TouchableOpacity>
+              <Text>Submit</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+});
